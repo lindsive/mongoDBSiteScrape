ffef2ca2a50ae62c45153fa0c446f8a3108c611b
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-10-06 16:45:28 -0400

    added connection to mongoose; created 'views' folder which holds pages and partials folder, pages- home.ejs and note.ejs, partials- footer.ejs, head.ejs, header.ejs; created assets folder with app.js and style.css; removed index.html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose  = require("mongoose");
 const logger = require("morgan");
+const ejs = require("ejs");
 
 // Scraping tools
 const axios = require("axios");
@@ -17,4 +18,33 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.set("view engine", ejs);
 
+// connect to mongoose
+mongoose.connect("mongodb://localhost/news-scrape", { useNewUrlParser: true });
+let mongooseCheck = mongoose.connection;
+
+// check mongoose connection
+mongooseCheck.on("error", console.error.bind(console, "connection error: "));
+mongooseCheck.once("open", () => {
+    console.log("connected to mongoose");
+});
+
+// routes
+app.get("/scrape", (req, res) => {
+    axios.get("https://www.nytimes.com/section/technology").then((response) => {
+        var $ = cheerio.load(response.data);
+
+        $("article div").each((i, element) => {
+            var result = {};
+
+            result.title = $(this)
+            .children("h3", "h2", )
+        })
+    })
+})
+
+// listen for port
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`)
+})
